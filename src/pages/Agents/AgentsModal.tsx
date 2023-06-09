893bd0f7f5ae10d3675ca531d29b35beb30df542
patrik.homola <patrik.homola@sonalake.com>
2023-06-09 16:20:30 +0200

    updated Agent modal in more declarative way

---
+++ src/pages/Agents/AgentsModal.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from 'react'
+import { FormikValues } from 'formik'
+import { useMutation, useQueryClient } from 'react-query'
+import { Form, Modal } from '../../components'
+import { Agent } from '../../models'
+import { agentFormFields, agentInitialValues } from '../../components/Form/formFields'
+import { createAgent, updateAgent } from '../../api/agents/agents.api'
+import { QueryKey } from '../../api/query-keys'
+
+interface AgentsModalProps {
+  isOpen: boolean
+  onClose: () => void
+  selectedAgent?: Agent
+}
+export const AgentsModal: FC<AgentsModalProps> = ({ isOpen, onClose, selectedAgent }) => {
+  const queryClient = useQueryClient()
+
+  const refetchAgentsAndClose = async () => {
+    await queryClient.invalidateQueries({ queryKey: QueryKey.AGENTS })
+    onClose()
+  }
+
+  const { mutateAsync: createNewAgent } = useMutation({
+    mutationFn: (agent: Omit<Agent, 'id'>) => createAgent(agent),
+    onSuccess: () => {
+      refetchAgentsAndClose()
+    },
+  })
+
+  const { mutateAsync: updateExistingAgent } = useMutation({
+    mutationFn: (agent: Agent) => updateAgent(agent),
+    onSuccess: () => {
+      refetchAgentsAndClose()
+    },
+  })
+
+  const handleSubmit = async (formValues: FormikValues) => {
+    // @TODO Formik needs to be generic
+    const newAgentvalues = formValues as unknown as Omit<Agent, 'id'>
+    const agentValues = formValues as unknown as Agent
+    selectedAgent ? updateExistingAgent(agentValues) : createNewAgent(newAgentvalues)
+  }
+
+  return (
+    <Modal isOpen={isOpen} title={selectedAgent ? 'Update agent' : 'Add new agent'} onClose={onClose}>
+      <Form formFields={agentFormFields} initialValues={selectedAgent || agentInitialValues} onSubmit={handleSubmit} />
+    </Modal>
+  )
+}
