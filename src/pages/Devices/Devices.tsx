b4fe24d27e74ebeb3247450ec33b58206e9ad0d4
patrik.homola <patrik.homola@sonalake.com>
2023-06-12 11:49:26 +0200

    implemented fragments, new pagination, and new fetching hook to devices

--- src/pages/Devices/Devices.tsx
+++ src/pages/Devices/Devices.tsx
@@ -3,32 +3,33 @@ import { Button, Tooltip } from 'flowbite-react'
 import React, { useCallback, useMemo, useState } from 'react'
 import { HiOutlinePencil, HiPlay, HiPlusCircle, HiStop, HiTrash } from 'react-icons/hi'
 import { toast } from 'react-toastify'
-import { Alert, DataTable, Form, LoadingIndicator, Modal, PageWrapper, Pagination } from '../../components'
+import { Alert, DataTable, Form, LoadingIndicator, Modal, PageProps, PageWrapper, Pagination } from '../../components'
 import { devicesColumns } from '../../components/DataTable/tableColumns/devicesColumns'
 import { handleResource } from '../../components/DataTable/tableColumns/handleResource'
 import { deviceFormFields, deviceInitialValues } from '../../components/Form/formFields'
 import { PAGINATION_DEFAULT_PAGE_SIZE_OPTION } from '../../constants'
 import { useFetch } from '../../hooks'
-import { Device, ResourceResponse } from '../../models'
+import { Device, DevicesQueryParams, ResourceResponse } from '../../models'
 import { PageTitle } from '../../components/PageTitle/PageTitle'
 import { ButtonIcon } from '../../components/ButtonIcon/ButtonIcon'
+import { useFetchDevices } from '../../api/devices/devices.api'
 
 const resource = 'devices'
 
 export const Devices = () => {
   const [selectedDevices, setSelectedDevices] = useState<Array<Row<Device>>>([])
   const [selectedDevice, setSelectedDevice] = useState<Device>()
-  const [currentPage, setCurrentPage] = useState(1)
-  const [pageSize, setPageSize] = useState(PAGINATION_DEFAULT_PAGE_SIZE_OPTION)
   const [isModalVisible, setIsModalVisible] = useState(false)
 
-  const {
-    resource: devices,
-    isLoading,
-    error,
-    fetchData,
-  } = useFetch<ResourceResponse>(`/api/devices?page=${currentPage}&page_size=${pageSize}`)
+  const [deviceQueryParams, setDeviceQueryParams] = useState<DevicesQueryParams>({
+    page: 1,
+    pageSize: PAGINATION_DEFAULT_PAGE_SIZE_OPTION,
+  })
+  const handlePaginationChange = (pageProps: PageProps) => setDeviceQueryParams(pageProps)
+
   const { resource: agents } = useFetch<ResourceResponse>(`/api/agents`)
+
+  const { data: devices, isLoading } = useFetchDevices(deviceQueryParams)
 
   const onCloseModal = useCallback(() => {
     if (isModalVisible) {
@@ -68,130 +69,137 @@ export const Devices = () => {
     [],
   )
 
-  if (error) {
-    throw error
-  }
-
   return (
     <PageWrapper>
-      {isLoading && !devices?.items?.length && (
-        <div className="mt-64">
-          <LoadingIndicator />
-        </div>
-      )}
-
-      {!!devices && (
-        <>
-          <PageTitle>Devices</PageTitle>
-
-          <div className="flex items-center gap-1 justify-end mb-5">
-            <Button color="info" onClick={() => setIsModalVisible(true)}>
-              <ButtonIcon as={HiPlusCircle} />
-              Add
-            </Button>
-
-            <Button
-              color="success"
-              onClick={() =>
-                toast(
-                  <Alert
-                    color="success"
-                    message={`${selectedDevices.length ? 'The selected devices were started!' : 'All devices were started!'
-                      } - to be implemented`}
-                  />,
-                )
-              }
-            >
-              <ButtonIcon as={HiPlay} />
-              Start
-            </Button>
-
-            <Button
-              color="dark"
-              onClick={() =>
-                toast(
-                  <Alert
-                    color="success"
-                    message={`${selectedDevices.length ? 'The selected devices were stopped!' : 'All devices were stopped!'
-                      } - to be implemented`}
-                  />,
-                )
-              }
-            >
-              <ButtonIcon as={HiStop} />
-              Stop
-            </Button>
-
-            <Button
-              color="failure"
-              disabled={!selectedDevices.length}
-              onClick={() =>
-                confirm('Delete the selected devices? - to be implemented') &&
-                toast(<Alert color="success" message={'The selected devices were deleted! - to be implemented'} />)
-              }
-            >
-              <ButtonIcon as={HiTrash} />
-              Delete
-            </Button>
+      <>
+        {isLoading && !devices?.items?.length && (
+          <div className="mt-64">
+            <LoadingIndicator />
           </div>
+        )}
+      </>
 
-          {/* @TODO: make DataTable properly generic and remove these castings */}
-          <DataTable
-            data={devices.items}
-            columns={devicesColumns.concat(devicesActionsColumn) as Array<Row<Device>>}
-            isSelectable
-            onSelection={(selectedRows) => setSelectedDevices(selectedRows as Array<Row<Device>>)}
-          />
+      <>
+        {!!devices && (
+          <>
+            <PageTitle>Devices</PageTitle>
 
-          <Pagination
-            currentPage={currentPage}
-            onPageChange={(page) => setCurrentPage(page)}
-            pageSize={pageSize}
-            onPageSizeChange={(size) => setPageSize(size)}
-            totalCount={devices.count}
-            disabled={isLoading}
-          />
+            <div className="flex items-center gap-1 justify-end mb-5">
+              <Button color="info" onClick={() => setIsModalVisible(true)}>
+                <ButtonIcon as={HiPlusCircle} />
+                Add
+              </Button>
 
-          {(isModalVisible || !!selectedDevice) && agents?.items?.length && (
-            <Modal
-              isOpen={isModalVisible || !!selectedDevice}
-              title={selectedDevice ? 'Update device' : 'Add new device'}
-              onClose={onCloseModal}
-            >
-              <Form
-                formFields={deviceFormFields}
-                initialValues={
-                  selectedDevice || {
-                    ...deviceInitialValues,
-                    agent: { id: agents.items[0].id },
-                  }
+              <Button
+                color="success"
+                onClick={() =>
+                  toast(
+                    <Alert
+                      color="success"
+                      message={`${selectedDevices.length ? 'The selected devices were started!' : 'All devices were started!'
+                        } - to be implemented`}
+                    />,
+                  )
                 }
-                snmpInputs
-                onSubmit={async (formValues) => {
-                  if (selectedDevice) {
-                    await handleResource({
-                      resource,
-                      operation: 'put',
-                      id: selectedDevice.id,
-                      body: formValues,
-                    })
-                  } else {
-                    await handleResource({
-                      resource,
-                      operation: 'post',
-                      body: formValues,
-                    })
+              >
+                <ButtonIcon as={HiPlay} />
+                Start
+              </Button>
+
+              <Button
+                color="dark"
+                onClick={() =>
+                  toast(
+                    <Alert
+                      color="success"
+                      message={`${selectedDevices.length ? 'The selected devices were stopped!' : 'All devices were stopped!'
+                        } - to be implemented`}
+                    />,
+                  )
+                }
+              >
+                <ButtonIcon as={HiStop} />
+                Stop
+              </Button>
+
+              <Button
+                color="failure"
+                disabled={!selectedDevices.length}
+                onClick={() =>
+                  confirm('Delete the selected devices? - to be implemented') &&
+                  toast(<Alert color="success" message={'The selected devices were deleted! - to be implemented'} />)
+                }
+              >
+                <ButtonIcon as={HiTrash} />
+                Delete
+              </Button>
+            </div>
+
+            {/* @TODO: make DataTable properly generic and remove these castings */}
+            <DataTable
+              data={devices.items}
+              columns={devicesColumns.concat(devicesActionsColumn) as Array<Row<Device>>}
+              isSelectable
+              onSelection={(selectedRows) => setSelectedDevices(selectedRows as Array<Row<Device>>)}
+            />
+
+            <Pagination
+              onPaginationChange={handlePaginationChange}
+              totalCount={agents.count}
+              disabled={isLoading}
+              pageProps={deviceQueryParams}
+            />
+            {/* 
+            <Pagination
+              currentPage={currentPage}
+              onPageChange={(page) => setCurrentPage(page)}
+              pageSize={pageSize}
+              onPageSizeChange={(size) => setPageSize(size)}
+              totalCount={devices.count}
+              disabled={isLoading}
+            /> */}
+
+            {(isModalVisible || !!selectedDevice) && agents?.items?.length && (
+              <Modal
+                isOpen={isModalVisible || !!selectedDevice}
+                title={selectedDevice ? 'Update device' : 'Add new device'}
+                onClose={onCloseModal}
+              >
+                <Form
+                  formFields={deviceFormFields}
+                  initialValues={
+                    selectedDevice || {
+                      ...deviceInitialValues,
+                      agent: { id: agents.items[0].id },
+                    }
                   }
+                  snmpInputs
+                  onSubmit={async (formValues) => {
+                    if (selectedDevice) {
+                      await handleResource({
+                        resource,
+                        operation: 'put',
+                        id: selectedDevice.id,
+                        body: formValues,
+                      })
+                    } else {
+                      await handleResource({
+                        resource,
+                        operation: 'post',
+                        body: formValues,
+                      })
+                    }
 
-                  fetchData()
+                    fetchData()
 
-                  onCloseModal()
-                }}
-              />
-            </Modal>
-          )}
-        </>
-      )}
+                    onCloseModal()
+                  }}
+                />
+              </Modal>
+            )}
+          </>
+        )}
+      </>
     </PageWrapper>
   )
 }
