a441822b9bb4546f7517f6258be343bef010f786
patrik.homola <patrik.homola@sonalake.com>
2023-06-12 11:26:45 +0200

    added react fragments

--- src/pages/Agents/Agents.tsx
+++ src/pages/Agents/Agents.tsx
@@ -54,51 +54,55 @@ export const Agents = () => {
 
   return (
     <PageWrapper>
-      {isLoading && (
-        <div className="mt-64">
-          <LoadingIndicator />
-        </div>
-      )}
-
-      {!!agents && (
-        <>
-          <PageTitle>Agents</PageTitle>
-
-          <div className="flex items-center gap-1 justify-end mb-5">
-            <Button color="info" onClick={openModal}>
-              <ButtonIcon as={HiPlusCircle} />
-              Add
-            </Button>
-
-            <Button
-              color="failure"
-              disabled={!selectedAgents.length}
-              onClick={() =>
-                confirm('Delete the selected agents? - to be implemented') &&
-                toast(<Alert color="success" message={'The selected agents were deleted! - to be implemented'} />)
-              }
-            >
-              <ButtonIcon as={HiTrash} />
-              Delete
-            </Button>
+      <>
+        {isLoading && (
+          <div className="mt-64">
+            <LoadingIndicator />
           </div>
+        )}
+      </>
 
-          {/* @TODO: make DataTable properly generic and remove this casting */}
-          <DataTable
-            data={agents.items}
-            columns={agentsColumns.concat(agentActionsColumn) as []}
-            onSelection={(selectedRows) => setSelectedAgents(selectedRows as Array<Row<Agent>>)}
-            isSelectable
-          />
+      <>
+        {!!agents && (
+          <>
+            <PageTitle>Agents</PageTitle>
 
-          <Pagination
-            onPaginationChange={handlePaginationChange}
-            totalCount={agents.count}
-            disabled={isLoading}
-            pageProps={agentQueryParams}
-          />
-        </>
-      )}
+            <div className="flex items-center gap-1 justify-end mb-5">
+              <Button color="info" onClick={openModal}>
+                <ButtonIcon as={HiPlusCircle} />
+                Add
+              </Button>
+
+              <Button
+                color="failure"
+                disabled={!selectedAgents.length}
+                onClick={() =>
+                  confirm('Delete the selected agents? - to be implemented') &&
+                  toast(<Alert color="success" message={'The selected agents were deleted! - to be implemented'} />)
+                }
+              >
+                <ButtonIcon as={HiTrash} />
+                Delete
+              </Button>
+            </div>
+
+            {/* @TODO: make DataTable properly generic and remove this casting */}
+            <DataTable
+              data={agents.items}
+              columns={agentsColumns.concat(agentActionsColumn) as []}
+              onSelection={(selectedRows) => setSelectedAgents(selectedRows as Array<Row<Agent>>)}
+              isSelectable
+            />
+
+            <Pagination
+              onPaginationChange={handlePaginationChange}
+              totalCount={agents.count}
+              disabled={isLoading}
+              pageProps={agentQueryParams}
+            />
+          </>
+        )}
+      </>
 
       <AgentsModal isOpen={isModalOpen} onClose={onCloseModal} selectedAgent={selectedAgent} />
     </PageWrapper>
