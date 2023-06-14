3bf708ec50096466a48cac5272afa70e2d9dc4b6
patrik.homola <patrik.homola@sonalake.com>
2023-06-14 13:30:13 +0200

    removed unused constants - navbar links

--- src/constants.ts
+++ src/constants.ts
@@ -1,18 +1,3 @@
-export const NAVBAR_LINKS = [
-  {
-    href: '/',
-    label: 'Dashboard',
-  },
-  {
-    href: '/agents',
-    label: 'Agents',
-  },
-  {
-    href: '/devices',
-    label: 'Devices',
-  },
-]
-
 export const PAGINATION_DEFAULT_PAGE_SIZE_OPTION = 10
 export const PAGINATION_PAGE_SIZE_OPTIONS = [PAGINATION_DEFAULT_PAGE_SIZE_OPTION, 20, 50, 100]
 
