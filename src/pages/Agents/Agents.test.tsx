030a561e1aa3bb6d853a9c9078a202adde13ea90
patrik.homola <patrik.homola@sonalake.com>
2023-06-16 16:02:41 +0200

    agents tests improvement

--- src/pages/Agents/Agents.test.tsx
+++ src/pages/Agents/Agents.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import MockAdapter from 'axios-mock-adapter'
-import { mockAgents } from '../../utils/testUtils/mockAgents'
-import { render, screen } from '../../utils/testUtils/testUtils'
+import { fireEvent, render } from '@testing-library/react'
+import { agentsMockedResponse } from '../../utils/testUtils/mockAgents'
+import { render as customRender, screen, waitFor } from '../../utils/testUtils/testUtils'
 import { baseApi } from '../../api/api'
+import { App } from '../../App'
+import { devicesMockedResponse } from '../../utils/testUtils/mockDevices'
 import { Agents } from './Agents'
 
 describe('Agents', () => {
@@ -10,7 +13,8 @@ describe('Agents', () => {
 
   beforeEach(() => {
     baseApiMock = new MockAdapter(baseApi)
-    baseApiMock.onGet('api/agents').reply(200, mockAgents)
+    baseApiMock.onGet('api/agents').reply(200, agentsMockedResponse)
+    baseApiMock.onGet('api/devices').reply(200, devicesMockedResponse)
   })
 
   afterEach(() => {
@@ -18,14 +22,34 @@ describe('Agents', () => {
   })
 
   it('should render the component', async () => {
-    render(<Agents />)
+    customRender(<Agents />)
 
     expect(await screen.findByText('Add')).toBeInTheDocument()
   })
 
   it('should render the table rows', async () => {
-    render(<Agents />)
+    customRender(<Agents />)
 
     expect(await screen.findByText('No data to display')).toBeInTheDocument()
+  })
+
+  it('should automatically navigate to Agents after opening the app', async () => {
+    render(<App />)
+
+    await waitFor(() => {
+      expect(screen.getAllByText(/agents/i)).toHaveLength(2)
+    })
+  })
+
+  it('should navigate to devices on side nav panel clicking', async () => {
+    render(<App />)
+    await waitFor(() => {
+      expect(screen.getAllByText(/agents/i)).toHaveLength(2)
+    })
+    fireEvent.click(screen.getByText('Devices'))
+
+    await waitFor(() => {
+      expect(screen.getAllByText(/devices/i)).toHaveLength(2)
+    })
   })
 })
