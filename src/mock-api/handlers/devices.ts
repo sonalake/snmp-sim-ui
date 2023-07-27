ec370390a6a24ca13057ca912225f8a5f9334dd2
Gordon McGuire <gordon.mcguire@sonalake.com>
2023-07-27 17:21:15 +0100

    SNMP-23: Added options to mock api for returning error values.

--- src/mock-api/handlers/devices.ts
+++ src/mock-api/handlers/devices.ts
@@ -5,6 +5,13 @@ import { DEVICES } from 'mock-api/data/devices';
 export const handlers = [
   rest.get('/api/devices', (req, res, ctx) => {
     // @TODO - handle pagination/filtering/etc.
+
+    const search = req.url.searchParams.get('search');
+    if (search === '404') {
+      return res(ctx.delay(500), ctx.status(404));
+    } else if (search === '500') {
+      return res(ctx.delay(500), ctx.status(500));
+    }
     return res(
       ctx.delay(500),
       ctx.status(200),
