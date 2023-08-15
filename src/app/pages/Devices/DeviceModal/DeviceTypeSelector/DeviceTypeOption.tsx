2fe7a8badbb6bb3659ddc59f24d251c2d14d0e63
Gordon McGuire <gordon.mcguire@sonalake.com>
2023-08-15 12:14:02 +0100

    SNMP-13: Styling for DeviceTypeSelector.

--- src/app/pages/Devices/DeviceModal/DeviceTypeSelector/DeviceTypeOption.tsx
+++ src/app/pages/Devices/DeviceModal/DeviceTypeSelector/DeviceTypeOption.tsx
@@ -8,11 +8,13 @@ interface DeviceTypeOptionProps {
 
 export const DeviceTypeOption: FC<DeviceTypeOptionProps> = ({ deviceType }) => {
   return (
-    <div className='flex items-start justify-between rounded p-3 bg-blue-50 dark:bg-gray-700 text-blue-800 dark:text-white'>
+    <div className='flex items-center justify-between rounded-lg p-3 bg-blue-50 dark:bg-gray-700 text-blue-800 dark:text-white'>
       <span className='text-base font-bold'>{deviceType.name}</span>
-      <span className='text-xs leading-normal py-0.5 px-2.5 bg-blue-100 dark:bg-gray-800 dark:text-gray-300'>
-        {!!deviceType.deviceCount && `${deviceType.deviceCount} devices`}
-      </span>
+      {!!deviceType.deviceCount && (
+        <span className='text-xs leading-normal rounded-md py-0.5 px-2.5 bg-blue-100 dark:bg-gray-800 dark:text-gray-300'>
+          {deviceType.deviceCount} devices
+        </span>
+      )}
     </div>
   );
 };
--- src/app/pages/Devices/DeviceModal/DeviceTypeSelector/DeviceTypeSelector.tsx
+++ src/app/pages/Devices/DeviceModal/DeviceTypeSelector/DeviceTypeSelector.tsx
@@ -1,18 +1,37 @@
 import { useState } from 'react';
-import { Button } from 'flowbite-react';
+import { HiChevronDown } from 'react-icons/hi';
+import { Button, Label } from 'flowbite-react';
 
 import { DeviceTypeModal } from './DeviceTypeModal';
+
+const customTheme = {
+  base: 'group flex h-min items-center relative w-full text-center relative focus:z-10 focus:outline-none border',
+  color: {
+    info: 'bg-gray-50 border-gray-300 text-gray-600 enabled:hover:bg-gray-100 enabled:hover:text-gray-900 focus:border-cyan-500 focus:ring-cyan-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-400 dark:enabled:hover:text-white dark:enabled:hover:bg-gray-700 dark:focus:border-cyan-500 dark:focus:ring-cyan-500'
+  },
+  inner: {
+    base: 'flex flex-1 items-center justify-between transition-all duration-200'
+  },
+  size: {
+    md: 'text-sm p-2.5'
+  }
+};
 
 export const DeviceTypeSelector = () => {
   const [isDeviceTypeOpen, setIsDeviceTypeOpen] = useState(false);
   return (
     <>
-      <Button
-        className='bg-primary-700 dark:bg-primary-700 text-white'
-        onClick={() => setIsDeviceTypeOpen(true)}
-      >
-        Select from the list
-      </Button>
+      <div>
+        <div className='mb-2 block'>
+          <Label className='leading-normal dark:text-white' value='Type' />
+        </div>
+        <div className='flex'>
+          <Button theme={customTheme} color='info' onClick={() => setIsDeviceTypeOpen(true)}>
+            Select from the list
+            <HiChevronDown className='h-4 w-4' />
+          </Button>
+        </div>
+      </div>
       <DeviceTypeModal isOpen={isDeviceTypeOpen} onClose={() => setIsDeviceTypeOpen(false)} />
     </>
   );
