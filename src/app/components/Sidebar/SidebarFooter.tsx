76897e27506809cd1822fd9a58da8e13cd36e473
Gordon McGuire <gordon.mcguire@sonalake.com>
2023-08-21 12:42:15 +0100

    SNMP-26: Increased logo image size. SNMP-26: Removed logout and avatar from sidebar.

--- src/app/components/Sidebar/SidebarFooter.tsx
+++ src/app/components/Sidebar/SidebarFooter.tsx
@@ -1,29 +1,9 @@
 import { HiHeart } from 'react-icons/hi';
-import { Avatar } from 'flowbite-react';
 
 import GithubLogo from 'assets/github.svg';
 
-const customTheme = {
-  root: {
-    initials: {
-      text: 'font-medium text-gray-900 dark:text-gray-900 text-xs font-medium',
-      base: 'inline-flex overflow-hidden relative justify-center items-center bg-gray-100 dark:bg-gray-100'
-    }
-  }
-};
-
 export const SidebarFooter = () => (
   <div className='flex flex-col gap-4'>
-    <div className='flex gap-2 items-center'>
-      <Avatar theme={customTheme} alt='PH' placeholderInitials='PH' rounded size='sm' />
-      <div className='h-full border-l-[1px] border-gray-200 dark:border-gray-700' />
-      <a
-        href='#'
-        className='font-medium text-sm hover:underline text-primary-700 dark:text-primary-200'
-      >
-        Logout
-      </a>
-    </div>
     <div className='flex justify-between items-center'>
       <p className='text-xs leading-normal text-gray-400 dark:text-gray-400'>
         {'Made with '}
